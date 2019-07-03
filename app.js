bd5bc9c682a653331975f16dfb0f08791cf4d5b7
Mauricio <maudob@live.com>
2019-07-02 20:36:12 -0500

    final

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  let msg = "";
+  if(!req.query.nombre){
+    msg = '<h1>Hola desconocido</h1>';
+  } else {
+    msg = `<h1>Hola ${req.query.nombre}!</h1>`
+  }
+  res.send(msg);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
